785b8fc032eea91812e9d306cd539c84b11f508f
Niklas <nik.muic@gmail.com>
2012-11-25 02:23:42 +0700

    start using git

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def show
+		@post = Post.find(params[:id])
+
+
+	end
+
+	def create
+		@post = Post.new(params[:post])
+
+		if @post.save
+			redirect_to posts_path, :notice => "Your post was saved"
+		else
+			render "new"
+		end
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update_attributes(params[:post])
+			redirect_to posts_path, :notice => "Your post has been updated"
+		else
+			render "edit"
+		end
+
+	end
+
+	def destroy
+		
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ea0c30c60cea7a3963e2f04ab64b454422955cc1b4425af5fa94a6541761e5e4236f9607cf91e68ba211ea6c2cd2b9f3028e10d22d07d4ce90a55dd3e9f5ef19'
