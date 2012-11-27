879993879df0af5458dc232ff2dfac81db11c6ba
Niklas <nik.muic@gmail.com>
2012-11-28 04:10:17 +0700

    add comment schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126173954) do
+ActiveRecord::Schema.define(:version => 20121127194522) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
