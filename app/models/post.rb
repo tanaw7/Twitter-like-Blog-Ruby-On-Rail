18409c4fe23600d7b4a18b532dd9ec53a9b44fa6
Niklas <nik.muic@gmail.com>
2012-12-02 06:08:19 +0700

    add security to prevent curl, godspeed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :title, :user_id
 
   belongs_to :user
+
+  validates :user, presence: true
+  validates_presence_of :user_id, :on => :create
+  validates_presence_of :content, :on => :create
+  validates_presence_of :title, :on => :create
+
 end
