5ba707c47047a9ec1bd25cc4347cccae79f96cc7
Niklas <nik.muic@gmail.com>
2012-12-16 22:57:56 +0700

    rem vad

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
   
 
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  #validates_confirmation_of :password
+  #validates_presence_of :password, :on => :create
+  #validates_presence_of :email
+  #validates_uniqueness_of :email
   
   def self.authenticate(email, password)
   user = find_by_email(email)
